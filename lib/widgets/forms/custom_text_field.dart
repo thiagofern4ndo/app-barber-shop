babdf2910f3da62c8e3fd4b98fd0a970c0618fec
Thiago F <thiagofern4ndo@gmail.com>
2025-02-19 20:30:44 -0300

    add button and form

---
+++ lib/widgets/forms/custom_text_field.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart'; // Certifique-se de importar o GoogleFonts
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+
+  const CustomTextField({super.key, required this.controller, required this.hintText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 60,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(25),
+        border: Border.all(
+          color: const Color(0xFF00FFB4),
+          width: 4,
+        ),
+      ),
+      child: TextField(
+        controller: controller,
+        style: GoogleFonts.poppins(
+          fontWeight: FontWeight.w600, 
+          fontSize: 22, 
+          color: Colors.white,
+        ),
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: GoogleFonts.poppins(
+            fontWeight: FontWeight.w600, 
+            fontSize: 22, 
+            color: const Color(0xFFB0B0B0), 
+          ),
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12), 
+          focusedBorder: InputBorder.none,
+          enabledBorder: InputBorder.none,
+        ),
+        textAlignVertical: TextAlignVertical.center, 
+      ),
+    );
+  }
+}
+
+
+
