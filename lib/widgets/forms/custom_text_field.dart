8684546abec1fed553b7ad601eb8a126df0765de
Thiago F <thiagofern4ndo@gmail.com>
2025-02-20 06:19:49 -0300

    edit buttom

--- lib/widgets/forms/custom_text_field.dart
+++ lib/widgets/forms/custom_text_field.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart'; // Certifique-se de importar o GoogleFonts
+import 'package:google_fonts/google_fonts.dart'; 
 
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
