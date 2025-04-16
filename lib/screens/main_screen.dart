d9d0dc4c6ed62630b53a7f38a64fdf447d718111
THIAGO FERNANDO DE SOUZA <thiago_f_de-souza@estudante.sesisenai.org.br>
2025-04-16 14:47:53 -0300

    att main screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:app_barber_shop/components/shared/custom_scaffold.dart';
 import 'package:app_barber_shop/components/buttons/button_main.dart';
-import 'package:app_barber_shop/components/buttons/button_contact.dart';
 import 'package:app_barber_shop/components/theme/colors.dart';
 import 'package:app_barber_shop/components/theme/fonts.dart';
 import 'package:app_barber_shop/screens/login_screen.dart';
